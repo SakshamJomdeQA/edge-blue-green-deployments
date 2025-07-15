18549d41398f11585541e54ed16d93a02027ad69
SakshamJomdeQA <saksham.jomde@contentstack.com>
2025-07-15 20:44:35 +0530

    blue dd v2

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,21 +1,9 @@
 const blueDeploymentHost = "edge-blue-green-deployments-blue.devcontentstackapps.com";
 
-export default {
-  async fetch(request) {
-    const modifiedUrl = new URL(request.url);
+export default async function handler(request) {
+  const modifiedUrl = new URL(request.url);
     modifiedUrl.hostname = blueDeploymentHost;
 
-    console.log("Forcing route to: BLUE deployment ->", modifiedUrl.hostname);
-
-    const newRequest = new Request(modifiedUrl, request);
-
-    try {
-      const response = await fetch(newRequest);
-      console.log("Status from upstream (BLUE):", response.status);
-      return response;
-    } catch (error) {
-      console.error("Fetch to BLUE failed:", error.message);
-      return new Response("Error fetching blue deployment", { status: 502 });
-    }
-  }
-};
+  const newRequest = new Request(modifiedUrl, request);
+  return fetch(newRequest);
+}
