d193145c7369d7b3cc70c7c38b4b2a4656ba0b8d
SakshamJomdeQA <saksham.jomde@contentstack.com>
2025-07-15 22:14:39 +0530

    azure na

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,5 +1,5 @@
-const blueDeploymentHost = "edge-blue-green-deployments-blue.devcontentstackapps.com";
-const greenDeploymentHost = "edge-blue-green-deployments.devcontentstackapps.com";
+const blueDeploymentHost = "edge-blue-green-deployments.azstagcontentstackapps.com";
+const greenDeploymentHost = "edge-blue-green-deployments-green.azstagcontentstackapps.com";
 
 export default async function handler(request) {
   const randomNumber = Math.floor((Math.random() * 10) + 1);
